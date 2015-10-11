3da21f197c0ce72349b3bf4a3ef7a32e4accd59a
nikolaiblackie <nikolai@adaptiv.co.nz>
2015-10-11 15:55:37 +1300

    Documentation Tweaks

--- Main/AdtvDemo.WUS.WebAPI.Logistics/Controllers/ShippingController.cs
+++ Main/AdtvDemo.WUS.WebAPI.Logistics/Controllers/ShippingController.cs
@@ -10,7 +10,14 @@ namespace AdtvDemo.WUS.WebAPI.Logistics.Controllers
 {
     public class ShippingController : ApiController
     {
-        
+        /// <summary>
+        /// Dispatches order 
+        /// </summary>
+        /// <param name="request">Dispatch request</param>
+        /// <returns>identity to track order</returns>
+        /// <response code="200">If if found</response>
+        /// <response code="404">If not found</response>
+        /// <response code="500">Internal Server Error</response>
         [Route("api/Shipping/DispatchOrder")]
         [HttpPost]
         public string DispatchOrder([FromBody] DispatchRequest request)
@@ -18,7 +25,14 @@ namespace AdtvDemo.WUS.WebAPI.Logistics.Controllers
             return Guid.NewGuid().ToString();
         }
 
-       
+       /// <summary>
+       /// Tracking API to verify where product located
+       /// </summary>
+       /// <param name="orderId">Order to track</param>
+        /// <returns>Order status object confirming stage</returns>
+        /// <response code="200">If if found</response>
+        /// <response code="404">If not found</response>
+        /// <response code="500">Internal Server Error</response>
         [Route("api/Shipping/WheresMyOrder")]
          [HttpGet]
         public DispatchedOrderStatus WheresMyOrder(int orderId)
